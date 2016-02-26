3c58dcbee08a3d0af8ef63bcd90e8ae00ce6b356
SvyatoslavPozhydaev <svyatreal@gmail.com>
2016-02-26 15:06:43 +0300

    доработан главная страница

--- js/script.js
+++ js/script.js
@@ -1,9 +1,28 @@
 $(function(){
-    $("#main").onepage_scroll();
+    // скролы странцы
+    //$("#main").onepage_scroll();
+    $('#fullpage').fullpage({
+        navigation: true,
+        navigationPosition: 'right',
+        continuousVertical:true,
+        resize: false,
+        animateAnchor:false,
+        scrollOverflow: false,
+        autoScrolling:true,
+        responsive: 900,
+        fitSection: false
+    });
     
+    // обработка нажатия кнопок верхнего меню
     $(".navigation-list__button").on("click", function(btn){
         $(".navigation-list__button").removeClass("active");
         $(this).addClass("active");
+    })
+    
+    // нажатие стрелки кнопки 
+    $("#btn-arrow-down").on("click", function(){
+        //$("#main").moveDown();
+        $.fn.fullpage.moveSectionDown();
     })
 });
 

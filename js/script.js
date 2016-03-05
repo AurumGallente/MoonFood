20dae1d316f2daa20477880f13a61e78cff16c03
SvyatoslavPozhydaev <svyatreal@gmail.com>
2016-03-05 15:35:24 +0300

    переделка

--- js/script.js
+++ js/script.js
@@ -10,32 +10,103 @@ $(function(){
         scrollOverflow: false,
         autoScrolling:true,
         responsive: 900,
-        fitSection: false
+        fitSection: false,
+        onLeave: function(){
+            if($(".last-slide").hasClass("fp-completely")){
+                $("#btn-reviews").fadeOut(300, function(){
+                    $("#btn-arrow-down").fadeIn(300).addClass("visible-lg");; 
+                });
+            }else{
+                
+            }
+        },
+        afterLoad: function(){
+            if($(".last-slide").hasClass("active")){
+                if($(window).width() >= 1200){
+                    $("#btn-arrow-down").removeClass("visible-lg").fadeOut(300, function(){
+                        $("#btn-reviews").fadeIn(300);
+                    });
+                }else{
+                    $("#btn-reviews").fadeIn(300);
+                }
+            }else{
+                
+            }
+        }
     });
     
     if($('#fp-nav ul li').size() <= 1){
         $('#fp-nav').hide();
     }
     
-    // обработка нажатия кнопок верхнего меню
-    /*$(".navigation-list__button").on("click", function(btn){
-        $(".navigation-list__button").removeClass("active");
-        $(this).addClass("active");
-    })*/
     
-    // нажатие стрелки кнопки 
+    // нажатие стрелки вниз
     $("#btn-arrow-down").on("click", function(){
         //$("#main").moveDown();
         $.fn.fullpage.moveSectionDown();
     });
     
-    var resizeHeaderFooter = function(){
-        var width = $(window).width();
-        //console.log(width);
-        $("header").width( width );
-        $("footer").width( width );
+    //   слайдер  -  МНЕНИЕ СПЕЦИАЛИСТА          
+    var slidingSpecialist = function(curSlide){
+        var prewSlide = 0;
+        var countSlide = $("#slider-opinion .slider-on-page__slide").size();
+        if(curSlide == 0){
+            prewSlide = countSlide - 1;
+        }else{
+            prewSlide = curSlide - 1;
+        }
+        
+        var nextSlide = 0;
+        if(curSlide == (countSlide-1)){
+            nextSlide=0;
+        }else{
+            nextSlide = curSlide + 1;
+        }
+        
+        $("#slider-opinion #prew-slide span").empty().append($("#slider-opinion .slider-on-page__slide").eq(prewSlide).find('span.profession').html());
+        $("#slider-opinion #next-slide span").empty().append($("#slider-opinion .slider-on-page__slide").eq(nextSlide).find('span.profession').html());
     };
-    //resizeHeaderFooter();
-    //$( window ).resize( resizeHeaderFooter );
+    var slidingReviews = function(curSlide){
+        var prewSlide = 0;
+        var countSlide = $("#slider-reviews .slider-on-page__slide").size();
+        if(curSlide == 0){
+            prewSlide = countSlide - 1;
+        }else{
+            prewSlide = curSlide - 1;
+        }
+        
+        var nextSlide = 0;
+        if(curSlide == (countSlide-1)){
+            nextSlide=0;
+        }else{
+            nextSlide = curSlide + 1;
+        }
+        
+        $("#slider-reviews #prew-slide span").empty().append($("#slider-reviews .slider-on-page__slide").eq(prewSlide).find('span.name').html());
+        $("#slider-reviews #next-slide span").empty().append($("#slider-reviews .slider-on-page__slide").eq(nextSlide).find('span.name').html());
+    };
+    slidingSpecialist(0);
+    $("#slider-opinion").sliderOnPage(slidingSpecialist);
+    slidingReviews(0);
+    $("#slider-reviews").sliderOnPage(slidingSpecialist);
+    
+    // наведение на состав
+    $(".composition").hover(function(){
+        var $this = $(this);
+        var position = $this.position();
+        var parentHeight = $this.height();
+        var $descrWind = $this.find(".composition-description");
+        var descrWidth = $descrWind.width();
+        console.log(position);
+        if($descrWind.css('display') == 'none'){
+            //$descrWind.fadeIn(600).offset({top:position.top+parentHeight/2, left:position.left-descrWidth/4});
+            $descrWind.fadeIn(600).css({ "top" : position.top });
+        }
+    });
+    // клик по крестику, для закрытия окна описание состава
+    $(".composition-description__close").click(function(){
+        var $parent = $(this).parent();
+        $parent.fadeOut(600);
+    });
 });
 
---
+++ js/slider_on_page.js
@@ -0,0 +1,82 @@
+(function( $ ){
+    $.fn.sliderOnPage = function(callbackAfterSlide){
+        var _slideSpeed = 900;
+        var _slider = this;
+        var _curSlide = 0;
+        var _countSlide = this.find(".slider-on-page__slide").size();
+        
+        var animSlide = function(arrow){
+            //clearTimeout(slideTime);
+            _slider.find(".slider-on-page__slide").eq(_curSlide).fadeOut(_slideSpeed);
+            if(arrow == "next")
+            {
+                if(_curSlide == (_countSlide-1))
+                {
+                    _curSlide=0;
+                }
+                else
+                {
+                    _curSlide++;
+                }
+            }
+            else if(arrow == "prew")
+            {
+                if(_curSlide == 0)
+                {
+                    _curSlide=_countSlide-1;
+                }
+                else
+                {
+                    _curSlide-=1;
+                }
+            }
+            _slider.find(".slider-on-page__slide").eq(_curSlide).fadeIn(_slideSpeed, function(){
+                if(callbackAfterSlide) 
+                    callbackAfterSlide(_curSlide);
+            });
+        }
+        
+        if( _countSlide > 1){
+            var maxHeight = 0;
+
+            /*_slider.find(".slider-on-page__slide").each(function() {
+                console.log($(this).height());
+                maxHeight = Math.max( maxHeight, $(this).height() );
+            });
+            _slider.height(maxHeight);*/
+            
+            
+            _slider.find(".slider-on-page__slide").css({
+                "position" : "absolute",
+                "top" : "0",
+                "left" : "0"
+            })
+            .hide()
+            .eq(0)
+            .show();
+            
+            _slider.height(_slider.find(".slider-on-page__slide").height());
+            $( window ).resize(function(){
+                _slider.height(_slider.find(".slider-on-page__slide").height());
+            });
+            
+            _slider.find("#prew-slide").click(function(){
+                animSlide("prew");
+            });
+            _slider.find("#next-slide").click(function(){
+                animSlide("next");
+            });
+            
+            $("#small-slider-btn").find("#prew-small-btn").click(function(){
+                _slider.find("#prew-slide").click();
+            });
+            $("#small-slider-btn").find("#next-small-btn").click(function(){
+                _slider.find("#next-slide").click();
+            });
+        }else{
+            console.log("no have slids");
+        }
+        
+        return this;
+    };
+ })(jQuery);

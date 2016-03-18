10cd89b0aa4e4ee0e269adf9bc692a8a47876aa2
SvyatoslavPozhydaev <svyatreal@gmail.com>
2016-03-18 14:26:45 +0300

    переделка страницы заказа

--- templates/order.php
+++ templates/order.php
@@ -9,27 +9,6 @@
                     </div>
                     <div class="row">
                         <div class="col-md-6 no-padding-left no-padding-sm-right">
-                            <form action="" class=" order__form-contact">
-                                <h2 class="your-order__header">Заполните контактные данные</h2>
-                                <input name="fio" data-validation="length" data-validation-length="min5" class="order__input" type="input" placeholder="ФИО *"/>
-                                <!--input name="country" class="order__input" type="input" placeholder="страна"/-->
-                                <!--select name="country" class="js-select-country maxheight" data-placeholder="Cтрана"-->
-                                    <!-- list country -->
-                                    <!--<option></option>
-                                    <option>Страна 1</option>
-                                    <option>Страна 2</option>
-                                    <option>Страна 3</option>
-                                    <option>Страна 4</option>
-                                    <option>Страна 5</option>
-                                </select-->
-                                <!--input name="city" class="order__input" type="input" placeholder="город"/-->
-                                <input name="adress" class="order__input" type="input" placeholder="адрес *"/>
-                                <!--input name="index" class="order__input" type="input" placeholder="индекс"/-->
-                                <input name="phone" class="order__input" type="input" placeholder="телефон *"/>
-                                <input name="email" data-validation="email" class="order__input" type="input" placeholder="email *" style="margin: 0;"/>
-                            </form>
-                        </div>
-                        <div class="col-md-6 no-padding-right no-padding-sm-left">
                             <div class="your-order">
                                 <h2 class="your-order__header">Ваш заказ</h2>
                                 <div class="your-order__detail">
@@ -38,11 +17,11 @@
                                         <div class="your-order__descr">
                                             Moon Food &mdash; полноценный обед из трех блюд в одной бутылке.
                                         </div>
-                                        <div class="your-order__cost-info">
-                                            <div class="your-order__cost">Стоимость</div>
-                                            <div class="your-order__value text-right"><span id="order-value-cost">1200</span> руб</div>
-                                        </div>
                                     </div>
+                                </div>
+                                <div class="your-order__cost-info">
+                                    <div class="your-order__cost">Стоимость</div>
+                                    <div class="your-order__value text-right"><span id="order-value-cost">1200</span> руб</div>
                                 </div>
                                 <div class="your-order__select-count">
                                     <div class="form-group order__select-count select-down visible-lg">
@@ -63,14 +42,41 @@
                                     </div>
                                     
                                 </div>
-                                <div class="your-order__btn-accept">
-                                    <div class="btn order__order-btn">
-                                        Заказать
-                                    </div>
+                            </div>
+                        </div>
+                        <div class="col-md-6 no-padding-right no-padding-sm-left">
+                            <form action="" class=" order__form-contact">
+                                <h2 class="your-order__header">Заполните контактные данные</h2>
+                                <input name="fio" data-validation="length" data-validation-length="min5" class="order__input" type="input" placeholder="ФИО *"/>
+                                <!--input name="country" class="order__input" type="input" placeholder="страна"/-->
+                                <!--select name="country" class="js-select-country maxheight" data-placeholder="Cтрана"-->
+                                    <!-- list country -->
+                                    <!--<option></option>
+                                    <option>Страна 1</option>
+                                    <option>Страна 2</option>
+                                    <option>Страна 3</option>
+                                    <option>Страна 4</option>
+                                    <option>Страна 5</option>
+                                </select-->
+                                <!--input name="city" class="order__input" type="input" placeholder="город"/-->
+                                <input name="adress" class="order__input" type="input" placeholder="адрес *"/>
+                                <!--input name="index" class="order__input" type="input" placeholder="индекс"/-->
+                                <input name="phone" class="order__input" type="input" placeholder="телефон *"/>
+                                <input name="email" data-validation="email" class="order__input" type="input" placeholder="email *" style="margin: 0;"/>
+                            </form>
+                        </div>
+                    </div>
+                    
+                    <div class="row">
+                        <div class="col-xs-12 col-md-6 col-md-offset-3 col-lg-4 col-lg-offset-4 no-padding">
+                            <div class="your-order__btn-accept">
+                                <div class="btn order__order-btn">
+                                    Заказать
                                 </div>
                             </div>
                         </div>
                     </div>
+                    
                 </div>
             </div>
         </section>

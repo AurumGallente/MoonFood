53796923ff46cbd9e2ecbeda7f9e6859697cd0cb
Сергей Цибульченко <sc@rocketstation.ru>
2016-03-21 10:46:22 +0300

    started to work on admin pannel

--- admin/login.php
+++ admin/login.php
@@ -1,109 +1,30 @@
-<?php
-// Страница авторизации
-
-# Функция для генерации случайной строки
-function generateCode($length=6) {
-    $chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHI JKLMNOPRQSTUVWXYZ0123456789";
-    $code = "";
-    $clen = strlen($chars) - 1;
-    while (strlen($code) < $length) {
-            $code .= $chars[mt_rand(0,$clen)];
-    }
-    return $code;
-}
-
-include ('connection.php');
-
-if (isset($_COOKIE['id']) and isset($_COOKIE['hash']))
-{
-    header("Location: admin.php"); exit();
-}
-
-if(isset($_POST['submit']))
-{
-    # Вытаскиваем из БД запись, у которой логин равняеться введенному
-    $query = mysqli_query($link,"SELECT user_id, user_password FROM users WHERE user_login='".mysqli_real_escape_string($link,$_POST['login'])."' LIMIT 1");
-    $data = mysqli_fetch_assoc($query);
-
-    # Сравниваем пароли
-    if($data['user_password'] === md5($_POST['password']))
-    {
-        # Генерируем случайное число и шифруем его
-        $hash = md5(generateCode(10));
-
-        if(!@$_POST['not_attach_ip'])
-        {
-            # Если пользователя выбрал привязку к IP
-            # Переводим IP в строку
-            $insip = ", user_ip=INET_ATON('".$_SERVER['REMOTE_ADDR']."')";
-        }
-
-        # Записываем в БД новый хеш авторизации и IP
-        mysqli_query($link, "UPDATE users SET user_hash='".$hash."' ".$insip." WHERE user_id='".$data['user_id']."'");
-
-        # Ставим куки
-        setcookie("id", $data['user_id'], time()+60*60*24*30);
-        setcookie("hash", $hash, time()+60*60*24*30);
-
-        # Переадресовываем браузер на страницу проверки нашего скрипта
-        header("Location: admin.php"); exit();
-    }
-    else
-    {
-        print "Вы ввели неправильный логин/пароль";
-    }
-}else{
-    echo "чтото не так!";
-}
-?>
-
-
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Moon Food</title>
-
-    <!-- Normalize -->
-    <link href="../css/normalize.css" rel="stylesheet">
-    <!-- Bootstrap -->
-    <link href="../css/bootstrap.css" rel="stylesheet">
-
-    <!-- Moon Food style -->
-    <link href="../css/style.css" rel="stylesheet">
-
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-    <![endif]-->
-</head>
-
-<body>
-	
-    <section class="bg-section slide-bg section3" style="display: flex;">
-        <form method="POST" class=" order__form-contact" style="margin: auto; width: 500px;">
-            <h2 class="your-order__header" style="color: #fff;">Авторизация в админ панель</h2>
-            <input name="login" class="order__input" type="input" placeholder="Логин"/>
-            <input name="password" class="order__input" type="password" placeholder="Пароль"/>
-            <div class="your-order__btn-accept">
-                <button class="btn order__order-btn" name="submit" type="submit">
-                    Вход
-                </button>
-            </div>
-        </form>
-    </section>
-	
-
-    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
-    <!-- Include all compiled plugins (below), or include individual files as needed -->
-    <script src="../js/bootstrap.min.js"></script>
-
-</body>
-
-</html>
+<div class="row-fluid">
+    <form class="form-horizontal" name="loginform" action="auth.php" method="POST">
+     <div class="form-group">
+       <label for="login" class="col-sm-2 control-label">Login</label>
+       <div class="col-sm-10">
+           <input type="login" class="form-control" id="login" name="login" placeholder="login" required="required" />
+       </div>
+     </div>
+     <div class="form-group">
+       <label for="password" class="col-sm-2 control-label">Password</label>
+       <div class="col-sm-10">
+         <input type="password" class="form-control" id="password" name="password" placeholder="Password">
+       </div>
+     </div>
+     <div class="form-group">
+       <div class="col-sm-offset-2 col-sm-10">
+         <div class="checkbox">
+           <label>
+             <input type="checkbox"> Remember me
+           </label>
+         </div>
+       </div>
+     </div>
+     <div class="form-group">
+       <div class="col-sm-offset-2 col-sm-10">
+         <button type="submit" class="btn btn-default">Sign in</button>
+       </div>
+     </div>
+   </form>
+</div>
